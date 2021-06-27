5bfa87682e1adb61cae4f68d4539b0abde59def0
Alex Beattie <artrisanbranding@me.com>
2021-06-27 15:41:33 -0700

    third commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+// import ContactForm from '../form/contactform'
 
 export default function Home() {
   return (
@@ -11,9 +12,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+           to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
+        <ContactForm />
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
